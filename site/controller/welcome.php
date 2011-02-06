3406d2431ce0f827d43d13fd2cc8cccd6319247f
Piyush Mishra <me@piyushmishra.com>
2011-02-07 00:27:38 +0530

    modified:   engine 	modified:   index.php 	modified:   site/controller/welcome.php

--- site/controller/welcome.php
+++ site/controller/welcome.php
@@ -5,7 +5,7 @@ if ( ! defined('SB_ENGINE_PATH')) exit('No direct script access allowed');
  * @author Piyush Mishra<me[at]piyushmishra[dot]com>
  */
 
-class welcome extends \pb\controller
+class welcome extends \sb\controller
 {
 	function __construct()
 	{
@@ -15,13 +15,13 @@ class welcome extends \pb\controller
 	function index()
 	{
 		global $time1;
-		echo (timenow()-$time1)." secs from init <br />";
+		echo (microtime(true)-$time1)." secs from init <br />";
 		echo "Index Called <br /> Loading model...<br />";
 		$this->load->model('check');
 		echo "Success!!! <br /> Loading view...<br />";
 		$test=array('test'=>'test variable set!!<br />');
 		$this->load->view('check_test',$test);
-		echo (timenow()-$time1)." secs from init".PHP_EOL;
+		echo (microtime(true)-$time1)." secs from init".PHP_EOL;
 	}
 }
 
