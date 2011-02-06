5e215d5b7d641410e4655ab5b87958b812c186b6
Piyush <piyush@cio.bz>
2011-02-06 23:38:21 +0530

    new file:   .htaccess 	new file:   index.php 	new file:   readme.md 	new file:   site/config.php 	new file:   site/controller/welcome.php 	new file:   site/index.html 	new file:   site/model/check.php 	new file:   site/view/check_test.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$time1=microtime(true);
+/**
+ * @package sambhuti
+ * @author Piyush Mishra<me[at]piyushmishra[dot]com>
+ */
+
+$sb_engine='engine';
+/**
+ * $pb_apps['full_url']='relative/path/from/this/file'
+ * specific to generic to prevent overriding
+ * donot use a trailing slash
+ */
+$sb_apps['http://localhost/sambhuti']='site';
+/**
+ * Now let sambhuti handle the rest
+ */
+
+
+define('SB_ENGINE_PATH',realpath($sb_engine).'/');
+if(SB_ENGINE_PATH=='/')
+	exit('Please check your $sb_engine '.__FILE__);
+require_once SB_ENGINE_PATH.'init.php';
+
+
+
+/**
+ * End of file Index
+ */
---
+++ site/config.php
@@ -0,0 +1,54 @@
+<?php
+if ( ! defined('SB_ENGINE_PATH')) exit('No direct script access allowed');
+/**
+ * @package sambhuti
+ * @author Piyush Mishra<me[at]piyushmishra[dot]com>
+ */
+
+/**
+ * @var $app_config[lazy_path] array
+ *				('namespace'=>array
+ * 					(
+ *						'/full/path1/',
+ *						'/full/path2/'
+ *					)
+ *				) 
+ *	Stores all Lazy load paths according to the namespaces they are in.
+ *	We've kept the defaults here to make it easier to follow(Dont mess with them unless you are sure what you are doing)
+ *
+ * 	Note: In case you use 'sb' namespace in your application to 
+ *		override the default classes in sambhuti, use loader::addlazypath($path,true)
+ *		adding them here will make sambhuti look for them after it has looked into its own directories.
+ * 	Note: Order of namespaces donot affect the load time,
+ *		but you should really care about the order of paths within the namespaces,
+ *		Specially if you are going override older versions of some classes with new ones, higher in the hierarchy
+ */
+$app_config['lazy_path']['global']['model']=SB_APP_PATH.'model/';
+$app_config['lazy_path']['global']['controller']=SB_APP_PATH.'controller/';
+
+/**
+ * @var $app_config['view_path'] string path to the view folder
+ * @default $app_config['view_path']=SB_APP_PATH.'view/';
+ *
+ */
+$app_config['view_path']=SB_APP_PATH.'view/';
+
+/**
+ * Db config vars
+ */
+$app_config['db']['DAO_name']="/#type#//#class#/dao";
+$app_config['db']['type']='mysql';
+/**
+ * Conn params
+ */
+$app_config['db']['master']['host']='localhost';
+$app_config['db']['master']['dbname']='phpbull';
+$app_config['db']['master']['user']='root';
+$app_config['db']['master']['pass']='piyush';
+$app_config['db']['master']['options']=array();
+$app_config['db']['master']['prefix']='pb_';
+
+$app_config['default_controller']='welcome';
+/**
+ * End of file Config
+ */
