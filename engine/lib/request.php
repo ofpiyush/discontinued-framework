b916f18ef7c21af3212050c84197aba16b448c91
Piyush <piyush@cio.bz>
2011-02-07 23:33:40 +0530

    new file:   engine/core/SBException.php 	new file:   engine/core/SBbase.php 	new file:   engine/core/config.php 	new file:   engine/core/controller.php 	new file:   engine/core/load.php 	new file:   engine/core/model.php 	new file:   engine/core/request.php 	new file:   engine/core/sambhuti.php 	new file:   engine/core/uri.php 	new file:   engine/index.html 	new file:   engine/init.php 	new file:   engine/lib/dao.php 	new file:   engine/lib/database.php 	new file:   engine/lib/index.html 	new file:   engine/lib/notifier.php 	new file:   engine/lib/request.php 	new file:   engine/lib/session.php 	new file:   engine/lib/uploader.php 	new file:   engine/lib/validator.php 	new file:   engine/thirdparty/Pimple

---
+++ engine/lib/request.php
@@ -0,0 +1,24 @@
+<?php
+namespace sb;
+if ( ! defined('SB_ENGINE_PATH')) exit('No direct script access allowed');
+/**
+ * @author Piyush Mishra<me[at]piyushmishra[dot]com>
+ */
+
+class request
+{
+	function __construct()
+	{
+		
+	}
+	function get($key)
+	{
+		
+	}
+
+}
+
+/**
+ * End of file Request
+ */
+
