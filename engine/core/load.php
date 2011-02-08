409986105a51be37beb183b6d694575e2c96c5b8
Piyush <piyush@cio.bz>
2011-02-08 23:49:21 +0530

    modified:   engine/core/load.php

--- engine/core/load.php
+++ engine/core/load.php
@@ -4,6 +4,7 @@ if ( ! defined('SB_ENGINE_PATH')) exit('No direct script access allowed');
 /**
  * @package sambhuti
  * @author Piyush Mishra<me[at]piyushmishra[dot]com>
+ * @author Gabriel <thehobbit[at]primorial[dot]net>
  */
 
 class load
