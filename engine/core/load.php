4b395531a5a2169ab1ef1a5f33a449a7f088ee50
Piyush Mishra <me@piyushmishra.com>
2011-02-10 10:31:36 +0530

    modified:   engine/core/SBbase.php 	modified:   engine/core/load.php

--- engine/core/load.php
+++ engine/core/load.php
@@ -20,31 +20,28 @@ class load
 		sambhuti::autoload($model,'model');
 		$pimple=sambhuti::pimple();
 		$modname="_".$model;
-		$pimple->$modname=$pimple->asShared(function($p) use($model)
+		if(!isset($pimple->$modname))
+		{
+			$pimple->$modname=$pimple->asShared(function($p) use($model)
 			{
 				return new $model($p->config);
 			});
-		$pimple->controller->_cannula($model_orig,$pimple->$modname);
+			$pimple->controller->_cannula($model_orig,$pimple->$modname);
+		}
 	}
 	function view($pb_view,$array=array())
 	{
 		$pb_view_fn = sambhuti::getfullpath('view_path', $pb_view);
 		if (!is_array($array))
 			$array = array();
-		self::cleanrequire($pb_view_fn, $array);
+		self::CleanRequire($pb_view_fn, $array);
 	}
 	function helper($helper)
 	{
-		echo "helpers are not yet supported";
-		die();
+		
 	}
 
-	
-
-	// FIXME: iyush: Please put this function wherever you deem appropriate.
-	// This seems like a good place to me, please move it if you have
-	// something better in mind.
-	static function cleanrequire($file, $vars)
+	static function CleanRequire($file, $vars)
 	{
 		unset($file, $vars);
 		extract(func_get_arg(1));
