875d32f788d3f3a03777f64afbfdf19ebff5a941
Piyush Mishra <me@piyushmishra.com>
2011-03-13 11:30:15 +0530

    modified:   engine/core/SBException.php 	modified:   engine/core/SBbase.php 	modified:   engine/core/config.php 	modified:   engine/core/controller.php 	modified:   engine/core/input.php 	modified:   engine/core/load.php 	modified:   engine/core/model.php 	modified:   engine/core/request.php 	modified:   engine/core/sambhuti.php 	modified:   engine/core/uri.php 	modified:   engine/helpers/functions.php 	modified:   engine/init.php 	modified:   engine/lib/database.php 	modified:   engine/lib/notifier.php 	modified:   engine/lib/request.php 	modified:   engine/lib/session.php 	modified:   engine/lib/uploader.php 	modified:   engine/lib/validator.php 	modified:   index.php 	LICENSE

--- engine/lib/notifier.php
+++ engine/lib/notifier.php
@@ -2,8 +2,29 @@
 namespace sb;
 if ( ! defined('SB_ENGINE_PATH')) exit('No direct script access allowed');
 /**
- * @package sambhuti
- * @author Piyush Mishra<me[at]piyushmishra[dot]com>
+ * Sambhuti
+ * Copyright (C) 2010-2011  Piyush Mishra
+ *
+ * License:
+ * This file is part of Sambhuti (http://sambhuti.org)
+ * 
+ * Sambhuti is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Sambhuti is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Sambhuti.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Sambhuti
+ * @author Piyush Mishra <me[at]piyushmishra[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2010-2011 Piyush Mishra
  */
 
 abstract class notifier
