807389150ecfc684ce2b21b70ad7de274b547ccd
Piyush <piyush@cio.bz>
2011-06-28 22:01:38 +0530

    Do not throw errors

--- engine/model/request.php
+++ engine/model/request.php
@@ -59,7 +59,8 @@ class request
                     if(!array_key_exists('regexURL',$app))
                     {
                         $urla=parse_url($app['siteURL']);
-                        $urla['path']= trim($urla['path'],'/');
+                        if(isset($urla['path']))
+                            $urla['path']= trim($urla['path'],'/');
                         if(!isset($urla['port']))
                             $urla['port']='';
                         if($urla['scheme'] == $scheme && $urla['host']==$httpHost && $urla['port'] ==$httpPort)
