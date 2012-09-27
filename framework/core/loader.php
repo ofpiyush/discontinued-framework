075fa595a573d616344ced6176ea0c585d494e1b
Piyush <piyush@cio.bz>
2012-09-27 20:03:04 +0530

    request and core

---
+++ framework/core/loader.php
@@ -0,0 +1,87 @@
+<?php
+namespace sambhuti\core;
+if(!defined('SAMBHUTI_ROOT_PATH')) exit;
+/**
+ * Sambhuti
+ * Copyright (C) 2012-2013 Piyush
+ *
+ * License:
+ * This file is part of Sambhuti (http://sambhuti.org)
+ *
+ * Sambhuti is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sambhuti is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sambhuti.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Sambhuti
+ * @author Piyush<piyush[at]codeitout[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2012 Piyush
+ */
+class loader {
+
+    /**
+     * Paths for lazyloading
+     * @var array
+     */
+    private $lazypath = array();
+
+    function __construct() {
+        spl_autoload_register(array($this, 'auto'));
+    }
+
+    /**
+     * Autoloader to load the classes under all lazypaths
+     * @param string $classname name of the class to be loaded
+     */
+    function auto($class) {
+        if(class_exists($class))
+            return true;
+        $array = explode('\\',$class);
+        if(array_key_exists($array[0],$this->lazypath)) {
+            $array[0] = $this->lazypath[$array[0]];
+            $fullpath=implode($array,'/').'.php';
+            if(file_exists($fullpath)) {
+                require_once($fullpath);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Add single lazypath for autoloader
+     * @param string $namespace namespace for replacement
+     * @param string $path the full path to the directory to be added
+     * @param bool $prefix whether to add the path before others or after
+     */
+    function addLazyPath($namespace, $path) {
+        $path = rtrim($path,'/');
+        $this->lazypath[$namespace] = $path;
+        return $this;
+    }
+
+    function getLazyPath($key) {
+        if(!empty($this->lazypath[$key])) {
+            return $this->lazypath[$key];
+        }
+        return false;
+    }
+
+    function getLazyPaths() {
+        return $this->lazypath;
+    }
+
+    function __sleep() {
+        return array('lazypath');
+    }
+
+}
